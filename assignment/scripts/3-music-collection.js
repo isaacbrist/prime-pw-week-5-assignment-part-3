822a1a297083808981ddf8a2ffd267ce97ff97f6
Isaac Brist <isaacbrist@gmail.com>
2022-06-04 09:55:09 -0500

    created addToCollection function to take in an object and push it into an array. Return added.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+let collection=[];
+//function to add an object ontaining the title of an album, artist, and year it was pubished to an array
+//should then return with new object.
+function addToCollection(title, artist, year){
+collection.push({title, artist, year})
+return {title, artist, year};
+}
+//end of addToCollection
+
+//testing the addToCollection function
