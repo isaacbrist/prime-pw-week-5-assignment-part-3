a68350b3ab9806ea10ce8732725e36d1f5d25774
Isaac Brist <isaacbrist@gmail.com>
2022-06-04 10:22:01 -0500

    Added console.logs to add albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,4 +8,10 @@ return {title, artist, year};
 }
 //end of addToCollection
 
-//testing the addToCollection function
+//testing the addToCollection function, adding 6 objects
+console.log('I just got a new album:', addToCollection('Funeral', 'Arcade Fire', 2004));
+console.log('I just got a new album:', addToCollection('The Suburbs', 'Arcade Fire', 2010));
+console.log('I just got a new album:', addToCollection('Brightside', 'The Lumineers', 2022));
+console.log('I just got a new album:', addToCollection('Sigh No More', 'Mumford and Sons', 2009));
+console.log('I just got a new album:', addToCollection('My head is and Animal', 'Of Monsters and Men', 2011));
+console.log('I just got a new album:', addToCollection('Special Reserve', 'Gaelic Storm', 2003));
