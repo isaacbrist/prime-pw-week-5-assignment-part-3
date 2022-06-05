4f4c701f216731201f12d36bcf2a9b9aa5efb6ff
Isaac Brist <isaacbrist@gmail.com>
2022-06-05 11:52:31 -0500

    Fixed the showCollection function and added string interpolation. Fixed findByArtist function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,36 +22,30 @@ console.log('Here is my musical collection!', collection);
 //creating function to show collection
 function showCollection(array) {
     console.log(array.length);
-    for (let album of array);
-    return array;
+    for (let album of array){
+        console.log(`${album.title} by ${album.artist}, published in ${album.year}`)
+    };
 };
 
-//testing showCollection function. should looop through array and log 'TITLE by ARTIST, published in YEAR'
-console.log(showCollection(collection))
+//testing showCollection function. should loop through array and log 'TITLE by ARTIST, published in YEAR'
+showCollection(collection)
 
 function findByArtist(artist) {
-    {
-        return collection.filter(({ artist: a }) => a == artist).map(({ title }) => title);
+    let results = [];
+    for (let album of collection) {
+       if (album.artist === artist) {
+           results.push(album);
+       }
     }
+    console.log(results)
+    return results;
 }
-    //----broken code----
-    //let results = [];
-    //for (let i = 0; i < collection.length; i++) {
-    //    if (collection[i].artist === artist); {
-    //        results.push(collection[i].title);
-    //    }
-    //    return results;
-    //}
-//
-//}
-//----end broken code----
-
 
 //Testing findByArtist function. Should return the titles of the corresponding album. 
 //'Funeral'
 //'The Suburbs'
-console.log('Do I have any albums by Arcade Fire in my collection?', findByArtist('Arcade Fire'))
+console.log('Do I have any albums by Arcade Fire in my collection? Expecting 2 ', findByArtist('Arcade Fire'))
 //'My head is an animal'
-console.log('Do I have any albums by Of Monsters and Men in my collection?', findByArtist('Of Monsters and Men'))
+console.log('Do I have any albums by Of Monsters and Men in my collection? Execting 1', findByArtist('Of Monsters and Men'))
 //Returns with an empty array
-console.log('Do I have any albums by Simon and Garfunkel in my collection?', findByArtist('Simon and Garfunkel'))
+console.log('Do I have any albums by Simon and Garfunkel in my collection? Expecting []', findByArtist('Simon and Garfunkel'))
